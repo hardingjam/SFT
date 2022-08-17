9266ce94b1c761d0f6343c0f93d7a7bbede5eccf
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-08-17 16:36:36 +0400

    Initial commit. Create svelte app

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<main>
+  <div>
+      <img src="/SFT.svg" class="logo" alt="SFT Logo" />
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+
+</style>

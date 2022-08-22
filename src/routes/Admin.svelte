474908b16c658bedcdba9bf498fafa2e7d5f6653
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-08-22 12:15:29 +0400

    Create empty frame for admins panel

--- src/routes/Admin.svelte
+++ src/routes/Admin.svelte
@@ -1,10 +1,50 @@
 <script>
 
 </script>
-<div class="sft-setup-container">
-hi there
-
+<div class="sft-admin-container">
+  <div class="admin-header">
+    <span>Admin</span>
+    <button class="btn-back">Back</button>
+  </div>
+  <div class="roles-container">
+    <span class="warning">Important - Deleting or adding is permanent on the blockchain. If all role admins are removed  then it will be unrecoverable.</span>
+  </div>
 </div>
 <style>
+    .sft-admin-container {
+        width: 510px;
+        height: calc(100vh - 280px);
+        background: rgba(44, 44, 84, 0.33);
+        border-radius: 20px;
+        padding: 12px;
+    }
 
+    .admin-header {
+        color: #ffffff;
+        display: flex;
+        justify-content: space-between;
+        padding: 5px 25px 10px 35px;
+    }
+
+    .btn-back {
+        box-sizing: border-box;
+        border: 1px solid #FFFFFF;
+        border-radius: 5px;
+        background: transparent;
+        color: #ffffff;
+    }
+
+    .roles-container {
+        height: calc(100% - 50px);
+        background: #FFFFFF;
+        border-radius: 10px;
+    }
+
+    .warning {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 8px;
+        line-height: 13px;
+        color: #000000;
+    }
 </style>

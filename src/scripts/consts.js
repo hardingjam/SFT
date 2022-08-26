9ce8ae8c783bd99ceee9d4b2626f7698630b5a60
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-08-26 15:04:22 +0400

    Add sg url

--- src/scripts/consts.js
+++ src/scripts/consts.js
@@ -3,4 +3,5 @@ export const CONTRACT_FACTORY_ADDRESS = {
     mumbai: "0xa40dD833D65a039668291A9BfADF74ca643f4E95",
     rinkeby: "0xEE95D90fCBDcFCCA629D3C56D733373C7B0121Ac"
 };
+export const SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/gild-lab/offchainassetvault"
 export const TEST_CONTRACT_ADDRESS = "0xb6eaFBfB1aC3f05C74CFB493Ce8DFda5427436C5";

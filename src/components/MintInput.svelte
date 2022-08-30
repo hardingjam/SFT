1cd18e1b93467c13d3bcb1a3ebab459f6d4df700
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-08-30 16:43:12 +0400

    Create mintInput component

---
+++ src/components/MintInput.svelte
@@ -0,0 +1,64 @@
+<script>
+    export let amount;
+</script>
+<div class="options-container">
+  <div class="options-background">
+    <div class="options">
+      <div class="labels">
+        <div class="txt">Mint Amount</div>
+        <div class="txt-options">Options</div>
+      </div>
+      <div class="options-input"><input type="text" bind:value={amount} autofocus></div>
+    </div>
+  </div>
+</div>
+
+<style>
+    .options-container {
+        width: 100%;
+    }
+
+    .options-background {
+        width: 100%;
+        padding: 7px 0;
+        background: linear-gradient(270deg, #342604 12.96%, #A28541 87.96%);
+    }
+
+    .options {
+        width: 100%;
+        background: #2C2C54;
+        padding: 15px 83px 15px 64px;
+        color: #ffffff;
+        display: flex;
+        justify-content: space-between;
+        align-items: end;
+    }
+
+    .labels {
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+    }
+
+    .txt {
+        font-weight: 300;
+        font-size: 16px;
+        line-height: 27px;
+    }
+
+    .txt-options {
+        font-weight: 300;
+        font-size: 32px;
+        line-height: 53px;
+    }
+
+    .options-input input {
+        font-weight: 500;
+        font-size: 32px;
+        line-height: 53px;
+        width: 100%;
+        text-align: right;
+        background: inherit;
+        color: inherit;
+    }
+</style>
--- src/routes/Mint.svelte
+++ src/routes/Mint.svelte
@@ -1,5 +1,6 @@
 <script>
     import {icons} from "../scripts/assets.js";
+    import MintInput from "../components/MintInput.svelte";
 
     let amount = 0;
 </script>
@@ -8,17 +9,7 @@
   <div class="audit-history btn-hover">
     <span>Audit History </span><img src={icons.show} alt="go to audit">
   </div>
-  <div class="options-container">
-    <div class="options-background">
-      <div class="options">
-        <div class="labels">
-          <div class="txt">Mint Amount</div>
-          <div class="txt-options">Options</div>
-        </div>
-        <div class="options-input"><input type="text" bind:value={amount} autofocus></div>
-      </div>
-    </div>
-  </div>
+  <MintInput bind:amount={amount}/>
 </div>
 
 <style>
@@ -42,52 +33,6 @@
         cursor: pointer;
     }
 
-    .options-container {
-        width: 100%;
-    }
 
-    .options-background {
-        width: 100%;
-        padding: 7px 0;
-        background: linear-gradient(270deg, #342604 12.96%, #A28541 87.96%);
-    }
-
-    .options {
-        width: 100%;
-        background: #2C2C54;
-        padding: 15px 83px 15px 64px;
-        color: #ffffff;
-        display: flex;
-        justify-content: space-between;
-        align-items: end;
-    }
-
-    .labels {
-        display: flex;
-        flex-direction: column;
-        text-align: left;
-    }
-
-    .txt {
-        font-weight: 300;
-        font-size: 16px;
-        line-height: 27px;
-    }
-
-    .txt-options {
-        font-weight: 300;
-        font-size: 32px;
-        line-height: 53px;
-    }
-
-    .options-input input {
-        font-weight: 500;
-        font-size: 32px;
-        line-height: 53px;
-        width: 100%;
-        text-align: right;
-        background: inherit;
-        color: inherit;
-    }
 </style>
 

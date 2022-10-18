0e1cbe8e8193285e3446b60468ba3e0906c6bcaa
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-10-18 18:13:45 +0400

    Add textarea, Show Highlighted text after paste

--- src/routes/NewSchema.svelte
+++ src/routes/NewSchema.svelte
@@ -1,120 +1,6 @@
 <script>
     import formatHighlight from 'json-format-highlight'
     import DefaultFrame from "../components/DefaultFrame.svelte";
-    import {afterUpdate} from "svelte";
-
-    let code = {
-        "$schema": "https:/json-schema.org/draft/2019-09/schema",
-        "$id": "http://example.com/example.json",
-        "type": "object",
-        "default": {},
-        "title": "Root Schema",
-        "required": [
-            "checked",
-            "dimensions",
-            "id",
-            "name",
-            "price",
-            "tags"
-        ],
-        "properties": {
-            "checked": {
-                "type": "boolean",
-                "default": false,
-                "title": "The checked Schema",
-                "examples": [
-                    false
-                ]
-            },
-            "dimensions": {
-                "type": "object",
-                "default": {},
-                "title": "The dimensions Schema",
-                "required": [
-                    "width",
-                    "height"
-                ],
-                "properties": {
-                    "width": {
-                        "type": "integer",
-                        "default": 0,
-                        "title": "The width Schema",
-                        "examples": [
-                            5
-                        ]
-                    },
-                    "height": {
-                        "type": "integer",
-                        "default": 0,
-                        "title": "The height Schema",
-                        "examples": [
-                            10
-                        ]
-                    }
-                },
-                "examples": [{
-                    "width": 5,
-                    "height": 10
-                }]
-            },
-            "id": {
-                "type": "integer",
-                "default": 0,
-                "title": "The id Schema",
-                "examples": [
-                    1
-                ]
-            },
-            "name": {
-                "type": "string",
-                "default": "",
-                "title": "The name Schema",
-                "examples": [
-                    "A green door"
-                ]
-            },
-            "price": {
-                "type": "number",
-                "default": 0.0,
-                "title": "The price Schema",
-                "examples": [
-                    12.5
-                ]
-            },
-            "tags": {
-                "type": "array",
-                "default": [],
-                "title": "The tags Schema",
-                "items": {
-                    "type": "string",
-                    "title": "A Schema",
-                    "examples": [
-                        "home",
-                        "green"
-                    ]
-                },
-                "examples": [
-                    ["home",
-                        "green"
-                    ]
-                ]
-            }
-        },
-        "examples": [{
-            "checked": false,
-            "dimensions": {
-                "width": 5,
-                "height": 10
-            },
-            "id": 1,
-            "name": "A green door",
-            "price": 12.5,
-            "tags": [
-                "home",
-                "green"
-            ]
-        }]
-    }
 
     let colors = {
         keyColor: 'black',
@@ -125,9 +11,16 @@
         nullColor: 'cornflowerblue'
     };
 
-    afterUpdate(() => {
-        document.getElementById("code").innerHTML = formatHighlight(code, colors)
-    })
+    function update(text) {
+        document.getElementById("highlighting-content").innerHTML = formatHighlight(text, colors)
+        document.getElementById("editing").style.display = "none"
+        document.getElementById("highlighting").style.display = "block"
+    }
+
+
+    let inputValue = ""
+
+    // $: inputValue && update(inputValue);
 
 </script>
 <DefaultFrame header="New Schema">
@@ -135,13 +28,12 @@
     <span class="f-weight-700">Schema label :</span> <input class="label-input"/>
 
     <div class="schema">
-      <!--      <Prism language="javascript" source="{JSON.stringify(code)}" />-->
-      <!--{JSON.stringify(code)}-->
+      <textarea class="textarea" id="editing" bind:value={inputValue}></textarea>
 
-      <pre><code id="code"></code>
-
-
+      <pre id="highlighting">
+        <code id="highlighting-content"></code>
       </pre>
+
     </div>
 
     <button class="default-btn btn-hover right">Deploy schema</button>
@@ -160,5 +52,19 @@
 
     .schema {
         height: 300px;
+        text-align: left;
+        overflow: auto;
+        margin: 10px 0;
     }
+
+
+    #editing {
+        width: 100%;
+        height: 100%;
+    }
+
+    #highlighting {
+        display: none;
+    }
+
 </style>

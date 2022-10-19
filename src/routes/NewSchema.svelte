c192461982cca94addbb2d01b6529fe2e2fbb0e6
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-10-19 11:40:52 +0400

    Deploy schema textbox style make it editable span for height auto increase

--- src/routes/NewSchema.svelte
+++ src/routes/NewSchema.svelte
@@ -22,21 +22,26 @@
 
     // $: inputValue && update(inputValue);
 
+
+    function deploySchema(){
+        console.log(document.getElementById("code").textContent)
+    }
+
 </script>
 <DefaultFrame header="New Schema">
   <div slot="content">
     <span class="f-weight-700">Schema label :</span> <input class="label-input"/>
 
     <div class="schema">
-      <textarea class="textarea" id="editing" bind:value={inputValue}></textarea>
-
+      <div class="editing-top-color">1</div>
+      <span class="textarea" role="textbox" id="code" contenteditable ></span>
       <pre id="highlighting">
         <code id="highlighting-content"></code>
       </pre>
 
     </div>
 
-    <button class="default-btn btn-hover right">Deploy schema</button>
+    <button class="default-btn btn-hover right" on:click={()=>{deploySchema()}}>Deploy schema</button>
   </div>
 
 </DefaultFrame>
@@ -51,16 +56,48 @@
     }
 
     .schema {
-        height: 300px;
         text-align: left;
         overflow: auto;
         margin: 10px 0;
+        position: relative;
     }
 
 
-    #editing {
+    .textarea {
         width: 100%;
         height: 100%;
+        border: none;
+        border-left: 35px solid #F0EFF1;
+        border-radius: 5px 5px 0 0;
+        border-bottom: 1px solid #D2D2D2;
+        background: #FBFBFB;
+        display: block;
+        overflow: hidden;
+        resize: both;
+        min-height: 300px;
+        line-height: 20px;
+        padding: 5px;
+        max-width: 675px;
+    }
+
+    .textarea:focus-visible{
+        outline:none
+    }
+
+    .textarea[contenteditable]:empty::before {
+        content: "Paste your schema here";
+        color: gray;
+    }
+
+
+    .editing-top-color{
+        position: absolute;
+        width: 35px;
+        left: 0;
+        top: 0;
+        background: #DCDBDD;
+        border-radius:5px 0 0 0;
+        text-align: center;
     }
 
     #highlighting {

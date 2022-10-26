274419a2194407b1050aa9a5b56080b8eca62e2a
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-10-26 10:28:53 +0400

    Import onmount and change class to id

--- src/routes/Test.svelte
+++ src/routes/Test.svelte
@@ -1,33 +1,34 @@
-
 <script type="text/javascript">
+    import {onMount} from "svelte";
 
-    console.log(document.getElementsByClassName('form'))
-    document.getElementsByClassName('form').jsonForm({
-        schema: {
-            name: {
-                type: 'string',
-                title: 'Name',
-                required: true
+    onMount(() => {
+        console.log(document.getElementById('form'));
+        document.getElementById('form').jsonForm({
+            schema: {
+                name: {
+                    type: 'string',
+                    title: 'Name',
+                    required: true
+                },
+                age: {
+                    type: 'number',
+                    title: 'Age'
+                }
             },
-            age: {
-                type: 'number',
-                title: 'Age'
+            onSubmit: function (errors, values) {
+                if (errors) {
+                    document.getElementById('res').innerHTML = '<p>I beg your pardon?</p>';
+                } else {
+                    document.getElementById('res').innerHTML = '<p>Hello ' + values.name + '.' +
+                        (values.age ? '<br/>You are ' + values.age + '.' : '') +
+                        '</p>';
+                }
             }
-        },
-        onSubmit: function (errors, values) {
-            if (errors) {
-                document.getElementById('res').innerHTML = '<p>I beg your pardon?</p>';
-            }
-            else {
-                document.getElementById('res').innerHTML ='<p>Hello ' + values.name + '.' +
-                    (values.age ? '<br/>You are ' + values.age + '.' : '') +
-                    '</p>';
-            }
-        }
-    });
+        });
+    })
 </script>
 <div>
   <h1>Getting started with JSON Form</h1>
-  <form class="form"></form>
+  <form id="form"></form>
   <div id="res" class="alert"></div>
 </div>

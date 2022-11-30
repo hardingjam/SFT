4bf5f5b980f6e51a76b05b0b45dc5bb6c3b373b9
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-11-30 13:38:10 +0400

    Add receipt audit component

---
+++ src/routes/ReceiptAudit.svelte
@@ -0,0 +1,96 @@
+<script>
+
+import DefaultFrame from "../components/DefaultFrame.svelte";
+import {navigateTo} from "yrv";
+
+export let receipt = {};
+
+let mockInfo =
+{
+    "producer_wallet": "1123",
+    "total_score": "1233",
+    "max_options": "2222",
+    "pie_certificate": "https://gildlab-ipfs.in.ngrok.io/ipfs/Qmd3MFNKPy3v835bXSTMArZcpEtRa4dWyD4Rje6nAcACU9"
+}
+
+let mockInfoArr = Object.keys(mockInfo).map(k=>{
+    return { key : k, value: mockInfo[k] }
+})
+
+</script>
+<DefaultFrame header="Audit History" >
+  <div slot="header-buttons" class="display-flex">
+    <button class="header-btn btn-hover" on:click={()=>{navigateTo("#admin")}}>Admins</button>
+  </div>
+  <div slot="content">
+    <div class="history">
+      <div class="receipts">
+        <table>
+          <tbody>
+          {#each mockInfoArr as info}
+            <tr class="tb-row">
+              <td>{info.key}</td>
+              <td>{info.value}</td>
+            </tr>
+          {/each}
+          </tbody>
+        </table>
+      </div>
+      <div class="error">
+      </div>
+    </div>
+  </div>
+</DefaultFrame>
+
+<style>
+
+    .history {
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+        width: 678px;
+        min-height: 530px;
+        position: relative;
+
+    }
+
+    table {
+        width: 100%;
+    }
+
+    thead, tbody {
+        text-align: center;
+    }
+
+    th, td {
+        text-align: left;
+    }
+
+    td {
+        padding-left: 10px;
+    }
+
+    .receipts {
+        height: 300px;
+        border-bottom: 1px solid #D2D2D2;
+        overflow: auto;
+    }
+
+    .receipts table th {
+        width: 33%;
+    }
+
+    input[type="date"]::-webkit-calendar-picker-indicator {
+        background: transparent;
+        bottom: 0;
+        color: transparent;
+        cursor: pointer;
+        height: auto;
+        left: 0;
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: auto;
+    }
+
+</style>

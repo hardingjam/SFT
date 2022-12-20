6d0f7135a3955ac18b322fd45a631f02605c8e10
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-12-20 16:47:13 +0400

    Setup sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import './app.css'
 import App from './App.svelte'
 
+import * as Sentry from "@sentry/svelte";
+import { BrowserTracing } from "@sentry/tracing";
+
+// Initialize the Sentry SDK here
+Sentry.init({
+  dsn: "https://fd58b636c0fc452585faa94df8775438@o4504354838544384.ingest.sentry.io/4504354840576000",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const app = new App({
   target: document.getElementById('app')
 })

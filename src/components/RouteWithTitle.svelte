ffb24890c4ba6ae0c73e4838bade24aa57f6728a
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-01-06 12:56:36 +0400

    Create RouteWithTitle.svelte component

---
+++ src/components/RouteWithTitle.svelte
@@ -0,0 +1,28 @@
+<div class="route-schema">
+  <div class="page-header-label">
+    <slot name="header">
+    </slot>
+  </div>
+  <slot name="content">
+  </slot>
+</div>
+
+<style>
+    .page-header-label {
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 27px;
+        color: #575757;
+        height: 77px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .route-schema {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        padding: 0 18px;
+    }
+</style>

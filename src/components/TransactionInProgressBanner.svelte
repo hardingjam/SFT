cb989d002189a992a5f4eb8b7578be2f85ad49ba
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-04-03 11:58:40 +0400

    Create transactionInProgress styles and texts

--- src/components/TransactionInProgressBanner.svelte
+++ src/components/TransactionInProgressBanner.svelte
@@ -1,10 +1,20 @@
 <script>
     import {transactionInProgress} from "../scripts/store.js";
+    import SftLoader from "./SftLoader.svelte";
+
+    export let topText = ""
+    export let bottomText = "View on the block explorer"
 </script>
 
 {#if (!$transactionInProgress)}
   <div class="frame">
-
+    <div class="content">
+      <div class="top-text">{topText}</div>
+      <SftLoader></SftLoader>
+      <div class="bottom-text">{bottomText}&nbsp; &nbsp;
+        <span class="icon"><slot name="icon"></slot></span>
+      </div>
+    </div>
   </div>
 {/if}
 
@@ -21,5 +31,27 @@
         width: 420px;
         height: 420px;
         z-index: 2;
+        padding: 10px;
     }
+
+    .content {
+        border-radius: 20px;
+        border: 1px solid rgba(255, 255, 255, 0.8);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        flex-direction: column;
+        color: #FFFFFF;
+        font-family: 'Mukta', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 33px;
+    }
+
+    .icon {
+        cursor: pointer;
+    }
+
 </style>

ba005d59be3cb70a56113337dc64800d5013770f
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-04-14 13:49:14 +0400

    should not have XSS vulnerabilities in JSON data

--- tests/component/NewSchema.cy.js
+++ tests/component/NewSchema.cy.js
@@ -4,4 +4,14 @@ describe('New asset class', () => {
     it('mounts NewSchema component', () => {
         cy.mount(NewSchema)
     })
+    it('should not have XSS vulnerabilities in JSON data', async () => {
+        cy.request('https://ipfs.io/ipfs/QmXw93e5TaiYfnqvtGSc7BTJiVTfa9QZHbk49uviQnvhTV')
+            .its('body')
+            .then((response) => {
+                // Check if response contains XSS vulnerabilities
+                const xssRegex = /<script\b[^>]*>([\s\S]*?)<\/script>/gm;
+                const responseString = JSON.stringify(response);
+                expect(responseString).not.to.match(xssRegex);
+            });
+        })
 })

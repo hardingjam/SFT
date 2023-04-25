3a588cbe8f6d08cd84d6b59b4e3b0fd18268b5c9
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-04-25 19:43:03 +0400

    Add show-hide to pass

--- src/routes/Ipfs.svelte
+++ src/routes/Ipfs.svelte
@@ -4,12 +4,26 @@
     let username = localStorage.getItem('ipfsUsername') || "";
     let password = localStorage.getItem('ipfsPassword') || "";
     let message = ""
+    let passwordInput;
+    let type = ""
+    let show = true
 
     function updateCredentials() {
         message = ""
         localStorage.setItem('ipfsUsername', username);
         localStorage.setItem('ipfsPassword', password);
         message = "Username and password has been updated";
+    }
+
+    $:type && (show = !show)
+
+    function toggleShowPassword() {
+        type = passwordInput.getAttribute("type")
+        if (type === "password") {
+            passwordInput.setAttribute("type", "text")
+        } else {
+            passwordInput.setAttribute("type", "password")
+        }
     }
 
 </script>
@@ -20,9 +34,12 @@
         <label class="mr-2">Username:</label>
         <input class="default-input" type="text" bind:value={username} autofocus/>
       </div>
-      <div class="display-flex space-between">
+      <div class="display-flex space-between password">
         <label class="mr-2">Password:</label>
-        <input class="default-input" type="password" bind:value={password}/>
+        <input class="default-input" type="password" bind:value={password} bind:this={passwordInput}/>
+        {#if password}
+          <div class="view" on:click={()=>{toggleShowPassword()}}>{show ? 'show' : 'hide'}</div>
+        {/if}
       </div>
       <button class="default-btn ok-button" disabled={!password || !username} on:click={()=>{updateCredentials()}}>OK
       </button>
@@ -43,6 +60,18 @@
         font-weight: 500;
         font-size: 16px;
         line-height: 27px;
+    }
+
+    .password {
+        position: relative;
+    }
+
+    .view {
+        position: absolute;
+        right: 5px;
+        font-weight: 400;
+        font-size: 14px;
+        cursor: pointer;
     }
 
 </style>

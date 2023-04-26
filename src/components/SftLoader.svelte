d66dee02a79f6443541fd12999f40d0cee4e74f5
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-04-26 18:57:18 +0400

    Add prereload to loader

--- src/components/SftLoader.svelte
+++ src/components/SftLoader.svelte
@@ -1,15 +1,29 @@
 <script>
-export let width = "200"
+    import {onMount} from "svelte";
+
+    export let width = "200"
+
+
+    onMount(async () => {
+        await loadAnimation('sftLoader.json');
+        const animation = document.getElementById('sft-loader');
+        animation.style.display = 'block';
+    });
+
+    const loadAnimation = (url) => {
+        return fetch(url).then((response) => response.json());
+    };
 
 </script>
 <div class="loader">
-  <lottie-player src="sftLoader.json" background="transparent"  speed="1"  style={`width: ${width}px; height: ${width}px;`} loop autoplay></lottie-player>
+  <lottie-player id="sft-loader" src="sftLoader.json" background="transparent" speed="1"
+                 style={`width: ${width}px; height: ${width}px;`} loop autoplay></lottie-player>
 </div>
 
 <style>
-  .loader{
-      display: flex;
-      justify-content: center;
-  }
+    .loader {
+        display: flex;
+        justify-content: center;
+    }
 </style>
 

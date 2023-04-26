451fd6f9aecbdfc0560b76e049d884a71ef3400c
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-04-26 19:18:41 +0400

    Try json instead path

--- src/components/SftLoader.svelte
+++ src/components/SftLoader.svelte
@@ -1,22 +1,10 @@
 <script>
-    import {onMount} from "svelte";
-
     export let width = "200"
-
-
-    onMount(async () => {
-        await loadAnimation('sftLoader.json');
-        const animation = document.getElementById('sft-loader');
-        animation.style.display = 'block';
-    });
-
-    const loadAnimation = (url) => {
-        return fetch(url).then((response) => response.json());
-    };
+    import sftLoader from "../../public/sftLoader.json"
 
 </script>
 <div class="loader">
-  <lottie-player id="sft-loader" src="sftLoader.json" background="transparent" speed="1"
+  <lottie-player id="sft-loader" src={sftLoader} background="transparent" speed="1"
                  style={`width: ${width}px; height: ${width}px;`} loop autoplay></lottie-player>
 </div>
 

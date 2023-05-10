b31109095930ad8a1190485682375b0df030aefe
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-05-10 17:08:00 +0400

    Uninstall flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 const config = {
   content: [
     "./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
   ],
 
   theme: {
     extend: {},
   },
 
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
   darkMode: 'class',
 };
 

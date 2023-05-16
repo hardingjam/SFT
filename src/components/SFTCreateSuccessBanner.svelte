04924e9069070bd7149677bd93f7594667007100
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-05-16 13:32:12 +0400

    Add height to prompt 445px

--- src/components/SFTCreateSuccessBanner.svelte
+++ src/components/SFTCreateSuccessBanner.svelte
@@ -83,7 +83,7 @@
         background: rgba(88, 85, 90, 0.92);
         border-radius: 20px;
         width: 616px;
-        height: 450px;
+        height: 445px;
         z-index: 2;
         padding: 10px;
     }

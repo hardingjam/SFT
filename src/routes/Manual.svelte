945a202fcfab0b9e12653e15fdca7374898acc23
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-05-19 20:01:52 +0400

    Pdf version

--- src/routes/Manual.svelte
+++ src/routes/Manual.svelte
@@ -1,20 +1,30 @@
 <script>
-    import {markdown} from '../assets/documents/gildlab-manual.js';
-    import {marked} from 'marked'
+    // import {markdown} from '../assets/documents/gildlab-manual.js';
+    // import {marked} from 'marked'
+
+    import manual from "../assets/documents/manual.pdf"
+
 
 </script>
 
 <div class="pdf-container">
-  <div class="preview">{@html marked(markdown)}</div>
+<!--  <div class="preview">{@html marked(markdown)}</div>-->
+  <iframe src={`${manual}#view=fit`} height="100%" width="100%"></iframe>
+
 </div>
 
 
 <style>
     .pdf-container {
-        background: #FFFFFF;
-        padding: 0 1rem;
-        text-align: left;
+        /*background: #FFFFFF;*/
+        /*padding: 0 1rem;*/
+        /*text-align: left;*/
+        /*width: calc(100vw - 223px);*/
+        /*margin-left: 203px;*/
+        /*margin-top: -48px;*/
+
         width: calc(100vw - 223px);
+        height: calc(100vh - 176px);
         margin-left: 203px;
         margin-top: -48px;
     }

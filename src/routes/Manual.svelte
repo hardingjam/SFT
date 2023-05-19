f7e86cabe89a19f236ab5f7af47533cdf669716a
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-05-19 12:24:06 +0400

    Import manual

--- src/routes/Manual.svelte
+++ src/routes/Manual.svelte
@@ -1,10 +1,17 @@
 <script>
-
+    import manual from "../assets/documents/manual.pdf"
 </script>
-<div class="manual">
-  Manual
+
+<div class="pdf-container">
+  <iframe src={`${manual}#view=fit`} height="100%" width="100%"></iframe>
 </div>
 
-<style>
 
+<style>
+    .pdf-container {
+        width: calc(100vw - 223px);
+        height: calc(100vh - 176px);
+        margin-left: 203px;
+        margin-top: -48px;
+    }
 </style>

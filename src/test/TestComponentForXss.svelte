3257206a2122f580931d566c395cdb607e3056ad
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-06-28 15:16:39 +0400

    Xss test

---
+++ src/test/TestComponentForXss.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let items = [];
+
+    let hasItems = items.length > 0;
+</script>
+
+{#if hasItems}
+  <ul>
+    {#each items as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
+{:else}
+  <p>No items available.</p>
+{/if}

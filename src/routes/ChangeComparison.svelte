350327e1af054ef6f48af28ac8320c527159d902
ninokeldishvili <ninokeldishvili@gmail.com>
2023-08-02 16:21:17 +0400

    Show icon in comparison

--- src/routes/ChangeComparison.svelte
+++ src/routes/ChangeComparison.svelte
@@ -2,21 +2,28 @@
     import {
         selectedReceiptInformations,
         activeNetwork,
-        sftInfo, pageTitle,
+        sftInfo, pageTitle, schemas,
     } from "../scripts/store";
     import {
         bytesToMeta,
-        cborDecode,
+        cborDecode, getIpfsGetWay,
         getSubgraphData, timeStampToDate, toSentenceCase,
     } from "../scripts/helpers.js";
     import {RECEIPT_INFORMATION_QUERY} from "../scripts/queries.js";
     import SftLoader from "../components/SftLoader.svelte";
+    import {IPFS_GETWAY, MAGIC_NUMBERS} from '../scripts/consts.js';
+    import axios from 'axios';
+    import {icons} from '../scripts/assets.js';
 
     let receiptInformation_before;
     let receiptInformation_after;
     let loading = false;
 
+    let schema;
+
     let comparisonTableData = [];
+
+    let fileProps = [];
 
     $:$activeNetwork && getInformations();
 
@@ -40,23 +47,35 @@
             receiptInformation_before = resp1.data?.receiptInformation || {}
             let resp2 = await getSubgraphData($activeNetwork, {id: information_after_id}, RECEIPT_INFORMATION_QUERY, 'receiptInformation')
             receiptInformation_after = resp2.data?.receiptInformation || {}
-            let decodedReceiptInformation_before = decodeInformation(receiptInformation_before.information)
-            let decodedReceiptInformation_after = decodeInformation(receiptInformation_after.information)
+            let decodedReceiptInformation_before = await decodeInformation(receiptInformation_before.information)
+            let decodedReceiptInformation_after = await decodeInformation(receiptInformation_after.information)
             let fieldsArray = [...Object.keys(decodedReceiptInformation_before),
                 ...Object.keys(decodedReceiptInformation_after)]
-
             //remove duplicate fields
             const uniqueSet = new Set(fieldsArray);
             fieldsArray = Array.from(uniqueSet);
 
             comparisonTableData = fieldsArray.map(field => {
-                let deference = highlight(decodedReceiptInformation_after[field], decodedReceiptInformation_before[field])
+                if (fileProps.indexOf(field) !== -1) {
+                    return {
+                        field: toSentenceCase(field),
+                        information_1: decodedReceiptInformation_before[field] ?
+                            `<div class="flex w-full items-center justify-center"><a href="${IPFS_GETWAY}${decodedReceiptInformation_before[field]}/" target="_blank" class="display-flex"><img class="action-icon ml-2" src="${icons.show}" alt="show"/></a></div>` :
+                            "",
+                        information_2: decodedReceiptInformation_after[field] ?
+                            `<div class="flex w-full items-center justify-center"><a href="${IPFS_GETWAY}${decodedReceiptInformation_after[field]}/" target="_blank" class="display-flex"><img class="action-icon ml-2" src="${icons.show}" alt="show"/></a></div>` :
+                            "",
+                    }
+                } else {
+                    let deference = highlight(decodedReceiptInformation_after[field], decodedReceiptInformation_before[field])
 
-                return {
-                    field: toSentenceCase(field),
-                    information_1: deference[0],
-                    information_2: deference[1],
+                    return {
+                        field: toSentenceCase(field),
+                        information_1: deference[0],
+                        information_2: deference[1],
+                    }
                 }
+
             })
         } catch (e) {
             console.log(e)
@@ -65,9 +84,33 @@
 
     }
 
-    function decodeInformation(information) {
+    async function decodeInformation(information) {
         let cborDecodedInformation = cborDecode(information.slice(18))
+        fileProps = await getFileProperties(cborDecodedInformation[0].get(MAGIC_NUMBERS.OA_SCHEMA))
         return bytesToMeta(cborDecodedInformation[0].get(0), "json")
+    }
+
+    async function getFileProperties(schemaHash) {
+        if (!schema && schemaHash && !schemaHash.includes(',')) {
+            let url = await getIpfsGetWay(schemaHash)
+            try {
+                if (url) {
+                    let res = await axios.get(url)
+                    if (res) {
+                        schema = res.data.schema
+                        //get schema properties that are file upload
+                        Object.keys(schema.properties).forEach(k => {
+                            if (schema.properties[k].editor === 'upload') {
+                                fileProps = [...fileProps, k]
+                            }
+                        })
+                    }
+                }
+            } catch (err) {
+                // console.log(err)
+            }
+        }
+        return fileProps
     }
 
     function highlight(newText, oldText) {

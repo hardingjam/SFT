772717dc5d65e1eeb636ee5911bbffa63ac94fc1
ninokeldishvili <ninokeldishvili@gmail.com>
2023-08-07 13:29:19 +0400

    Table wrapper

--- src/routes/AddressOverview.svelte
+++ src/routes/AddressOverview.svelte
@@ -1,8 +1,12 @@
 <script>
     import {sftInfo} from '../scripts/store.js';
     import SftLoader from '../components/SftLoader.svelte';
+    import {router} from 'yrv';
 
     let loading = false;
+
+    // let address=router
+    let address = $router.params.address
 
 </script>
 
@@ -11,31 +15,41 @@
     <SftLoader/>
   {/if}
   {#if !loading }
-    <div class="sft-table-container">
-      <table class="sft-table">
-        <thead>
-        <tr>
-          <th>Token name</th>
-          <th>Vault</th>
-          <th>Amount</th>
-          <th>Receipt ID</th>
-          <th>Revision ID</th>
-          <th>Date</th>
-        </tr>
-        </thead>
-        <tbody>
 
-        <tr>
-          <td>s</td>
-          <td>s</td>
-          <td>s</td>
-          <td>s</td>
-          <td>s</td>
-        </tr>
+    <div class="address-overview-container">
+      <div class="card-header justify-start">
+        <div class=""><b>Address</b> {address}</div>
+      </div>
+      <div class="address-overview-table">
+        <div class="sft-table-container">
+          <table class="sft-table">
+            <thead>
+            <tr>
+              <th>Action</th>
+              <th>Vault</th>
+              <th>Amount</th>
+              <th>Receipt ID</th>
+              <th>Revision ID</th>
+              <th>Date</th>
+            </tr>
+            </thead>
+            <tbody>
 
-        </tbody>
-      </table>
+            <tr>
+              <td>s</td>
+              <td>s</td>
+              <td>s</td>
+              <td>s</td>
+              <td>s</td>
+            </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+
+
   {/if}
 </div>
 <style>
@@ -47,5 +61,30 @@
         width: 100%;
         margin-right: 20px;
         margin-top: 102px;
+    }
+
+    .address-overview-container {
+        left: 420px;
+        top: 238px;
+        background: #FFFFFF;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        width: 100%;
+        height: 100vh;
+    }
+
+    .card-header {
+        gap: 13px;
+        padding: 13px 24px;
+        color: #575757;
+    }
+
+    .address-overview-table{
+        box-sizing: border-box;
+        border-radius: 10px;
+        padding: 24px 45px 50px 60px;
     }
 </style>

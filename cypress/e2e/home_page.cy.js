bdb135ca067154bafbd33c32996c5d184853f426
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-09-20 14:32:11 +0400

    Remove test test

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -7,10 +7,7 @@ describe('The Home Page', () => {
     beforeEach(() => {
         cy.visit('/')
     })
-    it('Check if connect-metamask-btn exists', () => {
-        // Load the component with the data
-        cy.get('.connect-metamask-btn').should('exist');
-    });    it('renders $tokens', () => {
+    it('renders $tokens', () => {
         // Load the component with the data
         cy.wait(1000)
         cy.get('.tokens').should('exist');

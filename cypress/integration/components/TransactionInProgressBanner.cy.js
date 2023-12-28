3c37b38d994656d0b8f98eaf816ca5eef0fab66e
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-12-28 13:53:23 +0400

    Update component test

--- cypress/integration/components/TransactionInProgressBanner.cy.js
+++ cypress/integration/components/TransactionInProgressBanner.cy.js
@@ -9,7 +9,8 @@ describe('Roles', () => {
 
     it(`Shows info text`, () => {
         cy.get('.contract-address').should('contain', 'View contract address');
-        cy.get('.copy-icon').should('exist');
+        cy.get('.copy-icon-tx').should('exist');
+        cy.get('.copy-icon-contract').should('exist');
     });
 
 });
